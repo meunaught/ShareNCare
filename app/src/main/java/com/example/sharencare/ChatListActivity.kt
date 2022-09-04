f3a4dbf9b44b1128af8d31f248dfd04f8897dd10
Mahadi Hasan <39445677+ratingminus@users.noreply.github.com>
2022-09-04 10:48:30 +0600

    chat list

--- app/src/main/java/com/example/sharencare/ChatListActivity.kt
+++ app/src/main/java/com/example/sharencare/ChatListActivity.kt
@@ -2,10 +2,55 @@ package com.example.sharencare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sharencare.Model.User
+import com.example.sharencare.adapter.ChatUserAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_search.*
 
 class ChatListActivity : AppCompatActivity() {
+
+    private lateinit var userRecyclerview : RecyclerView
+    private lateinit var userList : ArrayList<User>
+    private lateinit var adapter: ChatUserAdapter
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_list)
+        userList = ArrayList()
+        adapter = ChatUserAdapter(this, userList)
+        mAuth = FirebaseAuth.getInstance()
+
+        userRecyclerview = findViewById(R.id.userRecyclerView)
+        userRecyclerview.layoutManager = LinearLayoutManager(this)
+        userRecyclerview.adapter = adapter
+
+        val userRef = FirebaseDatabase.getInstance().getReference().child("Users")
+        userRef.addValueEventListener(object : ValueEventListener
+        {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                    userList?.clear()
+
+                    for(snapshot in dataSnapshot.children)
+                    {
+                        val user = snapshot.getValue(User::class.java)
+                        if(user!=null)
+                        {
+                            userList?.add(user)
+                        }
+                    }
+                    adapter?.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+        
     }
 }
