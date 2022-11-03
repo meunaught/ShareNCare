440e42529c928f2438c9daa24ff99f3aeb048552
ssharara123 <sharara99@gmail.com>
2022-11-04 01:34:50 +0600

    new changes

---
+++ app/src/main/java/com/example/sharencare/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.sharencare
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("DEPRECATION")
+class SplashScreen  : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
