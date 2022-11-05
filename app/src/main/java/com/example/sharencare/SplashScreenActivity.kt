45e7dfe8408a9555181b17e5cc4d4c9cc56591a7
Mahadi Hasan <39445677+ratingminus@users.noreply.github.com>
2022-11-05 15:39:10 +0600

    forced disabled dark mode

--- app/src/main/java/com/example/sharencare/SplashScreenActivity.kt
+++ app/src/main/java/com/example/sharencare/SplashScreenActivity.kt
@@ -5,6 +5,7 @@ import android.os.Handler
 import android.os.Looper
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 @Suppress("DEPRECATION")
 class SplashScreen  : AppCompatActivity() {
@@ -12,6 +13,7 @@ class SplashScreen  : AppCompatActivity() {
     lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); //force disable dark mode
         setContentView(R.layout.activity_splash_screen)
         handler = Handler()
         handler.postDelayed({
